52d99c4dfe62e417d10522a25232e2b9517d3c90
sollent <pavel.laikov98@gmail.com>
2020-04-12 01:12:30 +0300

    add

---
+++ src/Controller/User/SubscriptionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Entity\User\Subscription;
+use App\Entity\User\User;
+use App\Form\SubscriptionForm;
+use App\Serializer\CarPostSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SubscriptionController
+ */
+class SubscriptionController extends AbstractController
+{
+    /**
+     * @var CarPostSerializer
+     */
+    private $carPostSerializer;
+
+    /**
+     * @var array
+     */
+    private $subscriptionSerializable = [
+        'attribute' => [
+            'id',
+            'user' => [
+                'id',
+                'username',
+                'email'
+            ],
+            'mark' => [
+                'id',
+                'name'
+            ],
+            'model' => [
+                'id',
+                'name'
+            ],
+            'generation' => [
+                'id',
+                'name',
+                'fromYear',
+                'toYear'
+            ],
+            'active'
+        ]
+    ];
+
+
+    /**
+     * SubscriptionController constructor.
+     * @param CarPostSerializer $carPostSerializer
+     */
+    public function __construct(CarPostSerializer $carPostSerializer)
+    {
+        $this->carPostSerializer = $carPostSerializer;
+    }
+
+    /**
+     * @Route(
+     *     "/subscription/create"
+     * )
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     */
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $subscription = new Subscription();
+        $form = $this->createForm(SubscriptionForm::class, $subscription);
+
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Subscription $subscription */
+            $subscription = $form->getData();
+
+            $subscription->setUser(
+                $this->getDoctrine()->getRepository(User::class)->findOneBy(['username' => 'sollent'])
+            );
+            $subscription->setActive(true);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($subscription);
+            $em->flush();
+            $em->refresh($subscription);
+
+            return new JsonResponse($this->carPostSerializer->getSerializer()->normalize($subscription, null, $this->subscriptionSerializable));
+        }
+
+        return new JsonResponse(['error' => 'Some error'], Response::HTTP_BAD_REQUEST);
+    }
+}
---
+++ src/Form/SubscriptionForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User\Subscription;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class SubscriptionForm
+ */
+class SubscriptionForm extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mark', EntityType::class, [
+                'class' => 'App\Entity\CarMark',
+                'required' => true
+            ])
+            ->add('model', EntityType::class, [
+                'class' => 'App\Entity\CarModel',
+                'required' => false
+            ])
+            ->add('generation', EntityType::class, [
+                'class' => 'App\Entity\CarGeneration',
+                'required' => false
+            ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            'data_class' => Subscription::class
+        ]);
+    }
+}
